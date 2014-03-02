892521c73c609c5b14c30cabbe749abf06e757ea
kaitcart <kaitcart@gmail.com>
2014-03-01 18:35:24 -0800

    Update HCD.js

--- HCD.js
+++ HCD.js
@@ -1,51 +1,31 @@
 $(document).ready(function() {
-    $('h').click(function(){
-    	$(this).fadeOut('fast');
-    });
 
 	var bandCamp = "http://herecomedots.bandcamp.com/";  
 	var faceBook = "https://www.facebook.com/herecomedots"; 
 	var twitter = "https://twitter.com/AndHereComeDots";   
-	var photos = "HCD_Photos.html";
-	var shows = "UpcomingShows.html";
 
-	$('p3').click(function (){
+	$('p1').click(function (){
 		$(location).attr('href',bandCamp);
 	});
 
-	$('p3').mouseover(function() {
-		$(this).css('cursor', 'pointer');
-	});
+		$('p1').mouseover(function() {
+			$(this).css('cursor', 'pointer');
+		});
 	
-	$('p4').click(function (){
+	$('p2').click(function (){
 		$(location).attr('href',faceBook);
 	});
 
-	$('p4').mouseover(function() {
-		$(this).css('cursor', 'pointer');
-	});
+		$('p2').mouseover(function() {
+			$(this).css('cursor', 'pointer');
+		});
 	
-	$('p5').click(function (){
+	$('p3').click(function (){
 		$(location).attr('href',twitter);
 	});
-
-	$('p5').mouseover(function() {
-		$(this).css('cursor', 'pointer');
-	});
 	
-	$('p2').click(function (){
-		$(location).attr('href',photos);
-	});
-
-	$('p2').mouseover(function() {
-		$(this).css('cursor', 'pointer');
-	});
+		$('p3').mouseover(function() {
+			$(this).css('cursor', 'pointer');
+		});
 	
-	$('p1').click(function (){
-		$(location).attr('href',shows);
-	});
-	
-	$('p1').mouseover(function() {
-		$(this).css('cursor', 'pointer');
-	});
 });
