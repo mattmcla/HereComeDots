ce1712837bd268e2b9d6ceb78ed5d9f210a92fb0
Matt <mclaughlin.matt@gmail.com>
2014-03-21 20:21:47 +0000

    coffee edits

--- HCD.js
+++ HCD.js
@@ -1,31 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	var bandCamp = "http://herecomedots.bandcamp.com/";  
-	var faceBook = "https://www.facebook.com/herecomedots"; 
-	var twitter = "https://twitter.com/AndHereComeDots";   
+  var bandCamp = "http://herecomedots.bandcamp.com/";
+  var faceBook = "https://www.facebook.com/herecomedots";
+  var twitter = "https://twitter.com/AndHereComeDots";
 
-	$('p1').click(function (){
-		$(location).attr('href',bandCamp);
-	});
+  $('bc').click(function () {
+    $(location).attr('href', bandCamp);
+  });
 
-		$('p1').mouseover(function() {
-			$(this).css('cursor', 'pointer');
-		});
-	
-	$('p2').click(function (){
-		$(location).attr('href',faceBook);
-	});
+    $('tw').click(function () {
+    $(location).attr('href', faceBook);
+  });
 
-		$('p2').mouseover(function() {
-			$(this).css('cursor', 'pointer');
-		});
-	
-	$('p3').click(function (){
-		$(location).attr('href',twitter);
-	});
-	
-		$('p3').mouseover(function() {
-			$(this).css('cursor', 'pointer');
-		});
-	
+  $('tw').click(function () {
+    $(location).attr('href', twitter);
+  });
 });
